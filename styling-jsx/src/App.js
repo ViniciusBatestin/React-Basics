94e55593b020ea5cd04a33342980f2d7eb5f6708
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-09-22 20:04:45 +0200

    Styling JSX

---
+++ styling-jsx/src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+import Promo from "./Promo"
+
+function App () {
+  return <Promo heading="This is a test Heading" promoSubHeading="Promo Subheading test"/>
+}
+
+
+export default App;
