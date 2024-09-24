70e65d43d35c8c610231e609b49f7d96ce1af6fe
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-09-24 12:42:47 +0200

    Guess game, dynamic events LAB

--- dynamic-events/src/App.js
+++ dynamic-events/src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 import ModeToogle from './ModeToogle.js';
 
 function App() {
+  function clickHandle() {
+    let randomNum = Math.floor(Math.random() * 3) +1;
+    console.log(randomNum)
+    let userInput = prompt('type a number')
+    alert(`computer number: ${randomNum}, Your Guess: ${userInput}`)
+  }
+
   return (
     <>
       <Btn />
       <ModeToogle />
+      <button onClick={clickHandle}>Guess the number between 1 and 3</button>
     </>
   )
 }
