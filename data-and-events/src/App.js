d5415ba8d4195d5c68751c236f64d7a80ddef5d3
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-09-24 16:17:48 +0200

    Hooks

--- data-and-events/src/App.js
+++ data-and-events/src/App.js
@@ -1,11 +1,13 @@
 import Child from "./Child"
 import './App.css';
+import InputComponent from "./InputComponent";
 
 function App() {
   const date = new Date()
   return (
     <div className="App">
      <Child message={date.toLocaleTimeString()}/>
+     <InputComponent />
     </div>
   );
 }
