565efa496ed62c65c69088beaae6f5058427ffb0
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-09-24 12:22:24 +0200

    Improve with useState

--- dynamic-events/src/ModeToogle.js
+++ dynamic-events/src/ModeToogle.js
@@ -1,15 +1,13 @@
+import {useState} from 'react'
+
 function ModeToogle() {
-  let darkModeOn = false;
+  const [darkModeOn, setDarkModeOn] = useState(false);
   const darkMode = <h1>Dark mode is on</h1>;
   const lightMode = <h1>Light mode is on</h1>;
 
   function handleClick() {
-    darkModeOn = !darkModeOn
-    if (darkModeOn === true) {
-      console.log("Dark mode is on")
-    }else{
-      console.log("Light mode is on")
-    }
+    setDarkModeOn(!darkModeOn)
+    console.log(darkModeOn ? "Light mode is on" : "Dark Mode is on")
   }
   return (
     <div>
