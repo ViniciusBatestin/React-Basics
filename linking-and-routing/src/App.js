3dda16aa3aa08ed03ac985b112613abb5f2d8f56
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-10-10 19:22:04 +0200

    added Contact component

--- linking-and-routing/src/App.js
+++ linking-and-routing/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Homepage from "./Homepage";
 import AboutLittleLemon from "./AboutLittleLemon";
+import Contact from "./Contact";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -9,10 +10,12 @@ function App() {
 	  <nav>
       <Link to="/" className="nav-item">Homepage</Link>
       <Link to="/about" className="nav-item">About Little Lemon</Link>
+      <Link to="/contact" className="nav-item">Contact us</Link>
 	  </nav>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/about" element={<AboutLittleLemon />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </div>
   );
